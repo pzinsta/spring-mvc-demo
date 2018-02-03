06a360b124b13a58ebfca944ae0eb91077aed1e1
pzinsta <voodooshake@gmail.com>
2018-02-03 19:38:30 +0200

    Add @InitBinder example for trimming whitespace from the input

--- src/main/java/com/pzinsta/springdemo/mvc/CustomerController.java
+++ src/main/java/com/pzinsta/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.pzinsta.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor editor = new StringTrimmerEditor(false);
+        dataBinder.registerCustomEditor(String.class, editor);
+    }
+    
     @RequestMapping("/show-form")
     public String showForm(Model model) {
         model.addAttribute("customer", new Customer());
